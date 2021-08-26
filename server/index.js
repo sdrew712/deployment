fcfed61688348b95b97424f7e167983e5795778c
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-26 13:34:35 -0700

    added js file with link

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const path = require("path");
 
 const app = express();
 
+app.use('/css', express.static(path.join(__dirname, '../index.css')))
+
+app.use('/js', express.static(path.join(__dirname, '../main.js')))
+
 app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "../index.html"));
 });
-
-app.use('/css', express.static(path.join(__dirname, '../index.css')))
 
 const port = process.env.PORT || 4005;
 
