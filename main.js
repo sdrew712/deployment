a82e5c27e1654d127a29492a9adeba5e3f44a8a3
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-26 15:24:01 -0700

    made div with button that expands on scroll

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("sup world");
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const square = entry.target.querySelector('.square');
+
+    if (entry.isIntersecting) {
+      square.classList.add('square-animation');
+	  return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    square.classList.remove('square-animation');
+  });
+});
+
+observer.observe(document.querySelector('.square-wrapper'));
